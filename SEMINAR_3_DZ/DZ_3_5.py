2dcd09af976f89638a1e88f2df58cc9525d7ecd0
Solomkin Alexandr <solomin1995@mail.ru>
2022-12-25 14:57:06 +0300

    DZ_3_5: creating a function that returns a list of fibonacci numbers

--- SEMINAR_3_DZ/DZ_3_5.py
+++ SEMINAR_3_DZ/DZ_3_5.py
@@ -17,6 +17,25 @@
 
 num = int(input("Шаги в каждую сторону для Негафибоначчи: "))
 
+def fibonacci(count: int) -> list:
+    fibonacci_list = [0, 1]
+    # second_index = 1
+    new_element = 0
+
+    for i in range(count - 1):
+        new_element = fibonacci_list[i] + fibonacci_list[i + 1]
+        fibonacci_list.append(new_element)
+
+    return fibonacci_list
+
+
+# def negafibonacci(fib_list: list) -> list:
+#     negafibonacci_list = []
+
+f_list = fibonacci(num)
+
+print(f_list)
+
 
 
 # ============================================================================
