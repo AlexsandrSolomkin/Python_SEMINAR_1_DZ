96b3a68b18db2562a8b6e20f3e05838fa5341644
Solomkin Alexandr <solomin1995@mail.ru>
2022-12-28 01:32:05 +0300

    DZ_4_2: creating a function that outputs a list of prime factors

--- SEMINAR_4_DZ/DZ_4_2.py
+++ SEMINAR_4_DZ/DZ_4_2.py
@@ -24,6 +24,31 @@
 
 # Решение:
 
-num = input("Enter a natural number: ")
+num = int(input("Enter a natural number: "))
+
+def prime_factors(start_num: int) -> list:
+    list_p_f = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, \
+        53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    result_list = []
+
+    for i in range(len(list_p_f)):
+
+        while start_num % list_p_f[i] == 0:
+            if start_num % list_p_f[i] == 0:
+                result_list.append(list_p_f[i])
+                start_num /= list_p_f[i]
+            else:
+                continue
+        
+        if start_num == 1:
+            break
+
+    return result_list
+
+
+new_list = prime_factors(num)
+
+print(new_list)
+
 
 # =============================================================================
