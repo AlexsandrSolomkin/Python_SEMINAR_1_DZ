d2237446e1d84a9797d7f7903f813bb413a0bd0b
Solomkin Alexandr <solomin1995@mail.ru>
2022-12-29 05:09:29 +0300

    DZ_4_4: creating a method that enters data into a file

--- SEMINAR_4_DZ/DZ_4_4.py
+++ SEMINAR_4_DZ/DZ_4_4.py
@@ -25,5 +25,51 @@
 # Решение:
 
 
+def get_list_random_elements(length_list: int, min_el: int,
+                             max_el: int) -> list:
+
+    if length_list < 0:
+        # print("Negative value of the number of numbers!")
+        return []
+
+    import random
+    new_list = []
+
+    for i in range(length_list):
+        e = random.randint(min_el, max_el)
+        new_list.append(e)
+
+    return new_list
+
+
+def add_data(num: int, list_n: list, name_file: str) -> str:
+    import random
+
+    with open(name_file, "a", encoding="utf-8") as my_f:
+
+        for i in range(-num, 0):
+
+            plus_minus = random.randint(0, 1)
+
+            if list_n[i] > 0:
+                my_f.write(F"{list_n[i]}*x^{num}")
+
+                if plus_minus != 0:
+                    my_f.write(f" + ")
+                else:
+                    my_f.write(f" - ")
+
+            num -= 1
+
+        last_el = random.randint(1, 10)
+        my_f.write(f"{last_el} = 0\n")
+
+
+num_k = int(input("k: "))
+
+list_k = get_list_random_elements(num_k, 0, 10)
+add_new_data = add_data(num_k, list_k, "poly.txt")
+
+print(list_k)
 
 # =============================================================================
