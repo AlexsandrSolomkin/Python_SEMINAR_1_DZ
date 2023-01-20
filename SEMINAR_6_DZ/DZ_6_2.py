e31463201c0d890bb4f1b92adcfa4f0ec7974402
Solomkin Alexandr <solomin1995@mail.ru>
2023-01-21 01:51:11 +0300

    DZ_6_2: enter data

--- SEMINAR_6_DZ/DZ_6_2.py
+++ SEMINAR_6_DZ/DZ_6_2.py
@@ -15,6 +15,28 @@
 
 # Решение:
 
+num_1, num_2 = 20, 21
+min_n = 20
+max_n = int(input("Введите диапозон от {} до N, \
+    чтобы найти числа, кратные {} или {}".format(min_n, num_1, num_2)))
 
+
+# def get_random_el(start_data: int, min_el: int, max_el: int) -> int:
+#     el = 0
+
+#     if start_data < 0:
+#         return el
+
+#     import random
+
+#     el = random.randint(min_el, max_el)
+#     return el
+
+
+# list_n = [get_random_el(num, min_n, max_n) for i in range(num)]
+# list_new = [list_n[j] for j in range(1, len(list_n)) if list_n[j - 1] < list_n[j]]
+
+# print(list_n)
+# print(list_new)
 
 # =============================================================================
