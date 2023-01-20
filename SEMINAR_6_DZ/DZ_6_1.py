90c73184ee8deee5da469feb8173c883dd74ee5e
Solomkin Alexandr <solomin1995@mail.ru>
2023-01-21 01:08:25 +0300

    DZ_6_1: creating a method that outputs a random number from min to max

--- SEMINAR_6_DZ/DZ_6_1.py
+++ SEMINAR_6_DZ/DZ_6_1.py
@@ -20,5 +20,27 @@
 num = int(input("Введите сколько сгенерировать элементов в списке: "))
 
 
+def get_random_el(start_data: int, min_el: int, max_el: int) -> int:
+    el = 0
+
+    if start_data < 0:
+        print("Negative value of the number of numbers!")
+        return el
+
+    import random
+    
+    el = random.randint(min_el, max_el)
+    return el
+
+
+# def list_n() -> list:
+#     new_list = [f]
+
+#     for i in range(1, 101):
+#         if i % 2 == 0:
+#             list.append(i)
+
+#     print(list)
+
 
 # =============================================================================
