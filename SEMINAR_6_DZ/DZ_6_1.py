76e2f5d0f36a76198c8641dd8d40ecd777469fe4
Solomkin Alexandr <solomin1995@mail.ru>
2023-01-21 01:22:55 +0300

    DZ_6_1: creating list_comprehension

--- SEMINAR_6_DZ/DZ_6_1.py
+++ SEMINAR_6_DZ/DZ_6_1.py
@@ -18,29 +18,25 @@
 # Решение:
 
 num = int(input("Введите сколько сгенерировать элементов в списке: "))
+min_n = 1
+max_n = 100
 
 
 def get_random_el(start_data: int, min_el: int, max_el: int) -> int:
     el = 0
 
-    if start_data < 0:
+    if start_data < 0 or type(start_data) == str:
         print("Negative value of the number of numbers!")
         return el
 
     import random
-    
+
     el = random.randint(min_el, max_el)
     return el
 
 
-# def list_n() -> list:
-#     new_list = [f]
+list_n = [get_random_el(num, min_n, max_n) for i in range(num)]
 
-#     for i in range(1, 101):
-#         if i % 2 == 0:
-#             list.append(i)
-
-#     print(list)
-
+print(list_n)
 
 # =============================================================================
