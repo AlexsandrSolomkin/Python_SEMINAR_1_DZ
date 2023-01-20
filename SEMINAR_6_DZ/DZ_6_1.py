1762bf0ae35b2f01549f9d04cdad9eb1cd9a153a
Solomkin Alexandr <solomin1995@mail.ru>
2023-01-21 01:38:49 +0300

    DZ_6_1: creating list_comprehension_list_new

--- SEMINAR_6_DZ/DZ_6_1.py
+++ SEMINAR_6_DZ/DZ_6_1.py
@@ -25,8 +25,7 @@ max_n = 100
 def get_random_el(start_data: int, min_el: int, max_el: int) -> int:
     el = 0
 
-    if start_data < 0 or type(start_data) == str:
-        print("Negative value of the number of numbers!")
+    if start_data < 0:
         return el
 
     import random
@@ -36,7 +35,9 @@ def get_random_el(start_data: int, min_el: int, max_el: int) -> int:
 
 
 list_n = [get_random_el(num, min_n, max_n) for i in range(num)]
+list_new = [list_n[j] for j in range(1, len(list_n)) if list_n[j - 1] < list_n[j]]
 
 print(list_n)
+print(list_new)
 
 # =============================================================================
