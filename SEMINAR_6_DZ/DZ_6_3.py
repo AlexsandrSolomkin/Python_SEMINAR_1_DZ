d4fa7d43dad137cf32b62c4f4f665015c0fdb97f
Solomkin Alexandr <solomin1995@mail.ru>
2023-01-21 13:03:46 +0300

    DZ_6_3: enter data

--- SEMINAR_6_DZ/DZ_6_3.py
+++ SEMINAR_6_DZ/DZ_6_3.py
@@ -10,6 +10,34 @@
 
 # Решение:
 
+def input_str_in_list(stop: str) -> list:
+    list_data = []
+    el = ""
 
+    while True:
+        el = input(
+            "Enter the data for the list, to exit, enter '{}': ".format(stop))
+        if el == stop:
+            return list_data
+        else:
+            list_data.append(el)
+
+
+# def thesaurus(*args):
+#     names_dict = {}
+#     for i in sorted(args):
+#         letter = i[0]
+#         if letter not in names_dict:
+#             names_dict[letter] = [i]
+#         else:
+#             names_dict[letter] += [i]
+
+#     return names_dict
+
+
+new_list = input_str_in_list("exit")
+
+print(new_list)
+# print(thesaurus("Иван", "Мария", "Петр", "Илья", "Марина", "Алина", "Бибочка"))
 
 # =============================================================================
