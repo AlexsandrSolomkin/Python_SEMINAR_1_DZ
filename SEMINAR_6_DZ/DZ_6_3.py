7bf799ca951c2b6fdb58fc169dae04d14a442d9f
Solomkin Alexandr <solomin1995@mail.ru>
2023-01-24 01:01:54 +0300

    DZ_6_3: creating a function that generates lists of names with binding by the key of the first letter of the name

--- SEMINAR_6_DZ/DZ_6_3.py
+++ SEMINAR_6_DZ/DZ_6_3.py
@@ -10,6 +10,7 @@
 
 # Решение:
 
+
 def input_str_in_list(stop: str) -> list:
     list_data = []
     el = ""
@@ -23,21 +24,37 @@ def input_str_in_list(stop: str) -> list:
             list_data.append(el)
 
 
-# def thesaurus(*args):
-#     names_dict = {}
-#     for i in sorted(args):
-#         letter = i[0]
-#         if letter not in names_dict:
-#             names_dict[letter] = [i]
-#         else:
-#             names_dict[letter] += [i]
+def thesaurus(list_el: list) -> dict:
+    names_dict = {}
 
-#     return names_dict
+    for i in sorted(list_el):
+        # new_el_dict = {}
+        key = i[0]
+        # item = i
+
+        if key not in names_dict:
+            names_dict[key] = [i]
+            # .update({key: i})
+            # new_el_dict.update({key: i})
+            # names_dict = dict(names_dict.items() + new_el_dict.items())
+        else:
+            names_dict[key] += [i]
+
+    # for i in sorted(args):
+    #     letter = i[0]
+    #     if letter not in names_dict:
+    #         names_dict[letter] = [i]
+    #     else:
+    #         names_dict[letter] += [i]
+
+    return names_dict
 
 
 new_list = input_str_in_list("exit")
+new_args = thesaurus(new_list)
 
-print(new_list)
+# print(new_list)
+print(new_args)
 # print(thesaurus("Иван", "Мария", "Петр", "Илья", "Марина", "Алина", "Бибочка"))
 
 # =============================================================================
